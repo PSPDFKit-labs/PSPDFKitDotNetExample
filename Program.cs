55a548986a2f00ef52ff6a23b62fddb784f5a35e
Patrik Simic <patrik.simic@gmail.com>
2023-03-28 17:34:28 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,21 +7,25 @@ internal class Program
     private static void Main(string[] args)
     {
         PSPDFKit.Sdk.InitializeTrial();
-        string baseDir = AppDomain.CurrentDomain.BaseDirectory;
-        var fileProvider = new FileDataProvider(baseDir + "firstDocument.pdf");
-        var documentToEdit = new Document(fileProvider);
 
+        string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+        
+        var fileProvider = new FileDataProvider(baseDir + "firstDocument.pdf");
+        
+        var documentToEdit = new Document(fileProvider);
         
         documentToEdit.ImportDocumentJson(new FileDataProvider(baseDir + "instant.json"));
-
-        //documentToEdit.Save(new DocumentSaveOptions { flattenAnnotations = true });
         
         var documentEditor = documentToEdit.CreateDocumentEditor();
+        
         documentEditor.ImportDocument(0, DocumentEditor.IndexPosition.AfterIndex, new FileDataProvider("secondDocument.pdf"));
+        
         var filepath = baseDir + "documentEditorOutput.pdf";
+        
         documentEditor.SaveDocument(new FileDataProvider(filepath));
-
+        
         string outputJsonFileName = baseDir + "output.json";
+        
         documentToEdit.ExportDocumentJson(new FileDataProvider(outputJsonFileName));
     }
 }

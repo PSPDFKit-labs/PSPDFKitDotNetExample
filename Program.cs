b18e208b72cbe3502d3c6156bdafe59cba9107aa
Patrik Simic <patrik.simic@gmail.com>
2023-03-28 15:54:43 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using PSPDFKit;
+using PSPDFKit.Providers;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        PSPDFKit.Sdk.InitializeTrial();
+        string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+        var fileProvider = new FileDataProvider(baseDir + "firstDocument.pdf");
+        var documentToEdit = new Document(fileProvider);
+
+        
+        documentToEdit.ImportDocumentJson(new FileDataProvider(baseDir + "instant.json"));
+        
+        documentToEdit.Save(new DocumentSaveOptions { flattenAnnotations = true });
+        
+        var documentEditor = documentToEdit.CreateDocumentEditor();
+        documentEditor.ImportDocument(0, DocumentEditor.IndexPosition.BeforeIndex, new FileDataProvider("secondDocument.pdf"));
+        var filepath = baseDir + "documentEditorOutput.pdf";
+        documentEditor.SaveDocument(new FileDataProvider(filepath));
+    }
+}
